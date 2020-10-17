9db6c9e0f25dd11b8f7d4dc6bd2a391203f9743b
Aniket Gupta <63919139+aniketguptaa@users.noreply.github.com>
2020-10-17 23:38:53 +0530

    Create OddEven.py

---
+++ OddEven.py
@@ -0,0 +1,10 @@
+# Program to find the given number is odd or even
+
+number = input(int("Enter the number: "))
+
+# Logic of the program
+
+if (number % 2 == 0):
+  print("The given number is even")
+else:
+  print("The given number is odd")
